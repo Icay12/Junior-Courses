2dd6cd7fb083d92443fe37e9e3e09d2dd5a67d59
Icay12 <mymaiill@163.com>
2015-10-24 16:53:58 +0800

    add 1st version of lab 2

---
+++ Java/TechSupport/src/TechSupport/TechSupport.java
@@ -0,0 +1,42 @@
+package TechSupport;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.lang.String;
+
+class KeyWord{
+	private String m_word;
+	private ArrayList<String> m_response = new ArrayList<String>();
+	public KeyWord(){
+		
+	}
+	public void addWord(String word){
+		m_word = new String(word);
+	}
+	public void addResponse(String response){
+		m_response.add(response);
+	}
+	public String getResponse(){
+		
+	}
+	
+}
+
+public class TechSupport {
+
+	public ArrayList<String> questionSplit(String question){
+		
+	}
+	public ArrayList<String> findKeyWord(ArrayList<String> allWords){
+		
+	}
+	public void autoResponse(ArrayList<String> keywords){
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+	}
+
+}
